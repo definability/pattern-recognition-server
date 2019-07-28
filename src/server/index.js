c88ef83e1ec74b168752bcf698f3d0c273c2c786
char-lie <5962274+char-lie@users.noreply.github.com>
2019-07-28 17:34:23 +0300

    Refactor server code

--- src/server/index.js
+++ src/server/index.js
@@ -6,9 +6,11 @@ const port = process.env.PORT || 3000;
 const staticPath = path.join(__dirname, '..', '..', 'dist');
 
 app.use(express.static(staticPath));
+
 app.get('*', (req, res) => {
-   res.sendFile(path.join(staticPath, 'index.html'));
+  res.sendFile(path.join(staticPath, 'index.html'));
 });
+
 app.listen(port, () => {
   console.log(`Pattern recognition server is listening on port ${port}`);
 });
