a625b57f7d6a90f5d166e22622f31b3891e08ae7
char-lie <5962274+char-lie@users.noreply.github.com>
2019-08-17 09:19:05 +0300

    Fix path to executor from zero task executor

--- src/server/WSExecutorZero.js
+++ src/server/WSExecutorZero.js
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-const WSExecutor = require('../WSExecutor');
+const WSExecutor = require('./WSExecutor');
 
 /**
  * This client is able to send commands
