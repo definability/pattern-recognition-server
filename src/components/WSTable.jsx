26ea70908ad244aab1aeb42c635ee83671e3fc19
char-lie <5962274+char-lie@users.noreply.github.com>
2019-08-26 00:18:31 +0300

    Implement a WSTable component for WebSocket messages

---
+++ src/components/WSTable.jsx
@@ -0,0 +1,68 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2019 char-lie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
+
+class WSTable extends Component {
+  render() {
+    const { messages, sessionId } = this.props;
+    const messagesHtml = messages.map((message, i) => (
+      <tr>
+        <td>
+          {i}
+        </td>
+        <td>
+          {message.author}
+        </td>
+        <td>
+          {message.data}
+        </td>
+      </tr>
+    ));
+    const tableCaption = (sessionId
+      ? (<caption>Session {sessionId}</caption>)
+      : (<caption>Waiting for session to start</caption>)
+    );
+    const tableHeading = sessionId ? (
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Author</th>
+          <th>Message</th>
+        </tr>
+      </thead>
+    ) : (<thead />);
+    return (
+      <Table striped bordered hover>
+        {tableCaption}
+        {tableHeading}
+        <tbody>
+          {messagesHtml}
+        </tbody>
+      </Table>
+    );
+  }
+};
+
+export default WSTable;
