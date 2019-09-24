94ba4026b24c7220d23cc4d5f3515e4d5cab8ad9
char-lie <5962274+char-lie@users.noreply.github.com>
2019-09-24 22:43:59 +0300

    Fix variable name

--- src/server/WSExecutorFirst.js
+++ src/server/WSExecutorFirst.js
@@ -215,10 +215,10 @@ class WSExecutorFirst extends WSExecutor {
   onSetup(message) {
     const messageSplit = message.split(' ');
     if (!/^[\da-zA-Z\-\. ]+$/.test(message)) {
-      const wrongLetters = [...new Set(message.match(/[^\da-zA-Z\-\. ]/g))];
+      const wrongSymbols = [...new Set(message.match(/[^\da-zA-Z\-\. ]/g))];
       this.send(
         'Your message contains not allowed symbols: '
-        + wrongLetters.join(' ')
+        + wrongSymbols.join(' ')
         + `. Check whether you message "${message}" meets the needs.`
       );
       this.socket.close();
